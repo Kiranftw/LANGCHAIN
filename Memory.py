77b30cfd1a3820bc238c5ecaa65b018b7af1c7cd
UDAYKIRAN <uday33184@gmail.com>
2025-04-01 08:35:41 +0000

    create chatbot

--- Memory.py
+++ Memory.py
@@ -1,27 +1,43 @@
 from dotenv import load_dotenv, find_dotenv
-from langchain.memory import ConversationBufferMemory
 import os
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.conversation.base import ConversationChain
+from langchain.memory import ConversationSummaryMemory
+from langchain.prompts import PromptTemplate
 
 class Memory(object):
     def __init__(self) -> None:
         load_dotenv(find_dotenv())
         self.__API = os.getenv("GEMINIAPI")
-        genai.configure(api_key=self.__API)
-        self.memory = ConversationBufferMemory()
+        genai.configure(api_key=self.__API) 
         self.__MODEL: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(
-            model = "models/gemini-2.0-flash"
+            model="models/gemini-2.0-flash" 
         )
+        
+        prompt_template = PromptTemplate.from_template("User: {input}")
+        
+        self.memory = ConversationSummaryMemory(
+            llm=self.__MODEL, 
+            memory_key="history",
+            human_prefix="User",
+            ai_prefix="Bot",
+            input_key="input"
+        )
+        
         self.Conversation = ConversationChain(
-            llm = self.__MODEL,
-            memory = self.memory,
-            verbose = True
+            llm=self.__MODEL,  
+            memory=self.memory,  
+            verbose=True 
         )
     
-    def conversation(self) -> str:
-        print(self.Conversation.predict(input = "Hi there!"))
+    def start_conversation(self, user_input: str) -> str:
+        response = self.Conversation.predict(input=user_input)
+        return response
 
 if __name__ == "__main__":
-    Memory().Conversation()
+    chatbot = Memory() 
+    user_input = input()
+    response = chatbot.start_conversation(user_input) 
+    print(chatbot)
+    print(response)
