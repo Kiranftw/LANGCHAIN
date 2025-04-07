20bdf8ef6eee1e5361fbd3cf4d18ffd43df21d2a
UDAYKIRAN <uday33184@gmail.com>
2025-04-07 11:36:34 +0530

    implemented chains using langchain not learning inner working

--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_core.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 from dotenv import load_dotenv, find_dotenv
 import google.generativeai as genai
+from functools import wraps
 import os
 
 class Model():
@@ -18,7 +21,22 @@ class Model():
                 self.LLM = ChatGoogleGenerativeAI(
                     model= model.name
                 )
-                
+        self.PROJECTID = "langchain-42a07"
+        """
+        Connection between the Python and google firebase is Complicated will do it later.
+        """
+    
+    def ExceptionHandelling(func):
+        @wraps(func)
+        def Wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as E:
+                print(f"Error Occured {E}" )
+                return None
+        return Wrapper
+    
+    @ExceptionHandelling        
     def message(self):
         messages = [
             SystemMessage("You are an Expert in Social Media Content Strategy"),
@@ -26,6 +44,7 @@ class Model():
         ]
         result = self.LLM.invoke(messages)
     
+    @ExceptionHandelling
     def chatbot(self):
         self.History = []
         sysmessage = SystemMessage("You are an Helpfull AI Assistant")
@@ -41,7 +60,30 @@ class Model():
             self.History.append(AIMessage(content = response))
             print(f"AI; {response}")
             
+    @ExceptionHandelling
+    def promptTemplate(self):
+        template = """
+        Dump total knowledge on {topic} no bais and I want you to give me your opinion on 
+        this {skill}.
+        """
         
-
+        messages = [
+            ("system", "You are a comedian who tell jokes on pirticular god {topic}"),
+            ("human", "tell me {count} jokes")
+        ]
+        self.promptTemplate = ChatPromptTemplate.from_messages(messages)
+        prompt = self.promptTemplate.format_messages(topic="jesus", count="3")
+    
+    def chains(self):
+       promptTemplate = ChatPromptTemplate.from_messages(
+           [
+               ("system", """you are actually facts expert give me some facts
+               this {animal}."""),
+               ("human", """tell me about this {number} of facts.""")
+           ])
+       chain = promptTemplate | self.LLM | StrOutputParser()
+       result = chain.invoke({"animal" : "lion", "number" : 2})
+       print(result)
+   
 if __name__ == "__main__":
-    Model().chatbot()
+    Model().chains()
